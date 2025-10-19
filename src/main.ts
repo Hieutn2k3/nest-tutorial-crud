3cf007c5045a0240ba0aaaddd2d5fe5f35e767e4
hieutn <hieutran290611@javis.vn>
2025-10-19 20:10:10 +0700

    add DTO để validate dữ liệu đầu vào

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Tự động loại bỏ các field khi không dc khai báo  decorater
+      forbidNonWhitelisted: true, // Nếu field không được khai báo trong DTO thì sẽ báo lỗi
+      transform: true, //TỰ động chuyển hóa dữ liệu được khai báo trong DTO
+      exceptionFactory: (validationErrors) => {
+        console.log(validationErrors)
+        return new UnprocessableEntityException(
+          validationErrors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints as any).join(','),
+          })),
+        )
+      },
+    }),
+  )
   await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
